39d532acf53c2379ccaa2555face5f5975eab283
ozaner <56015962+ozaner@users.noreply.github.com>
2024-10-28 23:00:42 -0400

    Add new_http_wav fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
 mod streaming_wav;
 
 pub use streaming_wav::*;
+
+use reqwest::{Client, Url};
+use stream_download::{
+    http::HttpStream, storage::memory::MemoryStorageProvider, StreamDownload,
+    StreamInitializationError,
+};
+use thiserror::Error;
+
+pub async fn new_http_wav<S: WavSample>(
+    url: Url,
+) -> Result<StreamingWav<S, StreamDownload<MemoryStorageProvider>>, NewHttpWavError> {
+    let reader = StreamDownload::new_http(
+        url,
+        MemoryStorageProvider,
+        stream_download::Settings::default(),
+    )
+    .await?;
+
+    Ok(StreamingWav::new(reader)?)
+}
+
+#[derive(Debug, Error)]
+pub enum NewHttpWavError {
+    #[error("Error decoding the streamed wav file.")]
+    WavDecodingError(#[from] StreamingWavError),
+
+    #[error("Error initializing the stream.")]
+    StreamInitializationError(#[from] StreamInitializationError<HttpStream<Client>>),
+}
