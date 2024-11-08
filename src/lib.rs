4c6dbc66c134791bd8dba58495ed1ac56d7be2be
ozaner <56015962+ozaner@users.noreply.github.com>
2024-11-07 23:46:36 -0500

    Reuse Http client in get_wav_stream
    Also renamed get_wav_stream from new_http_wav.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod streaming_wav;
 
+pub use reqwest::Client; //Need to re-export since it's used in [`new_http_wav`]'s api.
 pub use streaming_wav::*;
 
-use reqwest::{Client, Url};
+use reqwest::Url;
 use stream_download::{
-    http::HttpStream, storage::memory::MemoryStorageProvider, StreamDownload,
-    StreamInitializationError,
+    http::{HttpStream, HttpStreamError},
+    storage::memory::MemoryStorageProvider,
+    StreamDownload, StreamInitializationError,
 };
 use thiserror::Error;
 
@@ -13,9 +15,12 @@ use thiserror::Error;
 #[allow(type_alias_bounds)]
 pub type HttpStreamingWav<S: WavSample> = StreamingWav<S, StreamDownload<MemoryStorageProvider>>;
 
-pub async fn new_http_wav<S: WavSample>(url: Url) -> Result<HttpStreamingWav<S>, NewHttpWavError> {
-    let reader = StreamDownload::new_http(
-        url,
+pub async fn get_wav_stream<S: WavSample>(
+    client: Client,
+    url: Url,
+) -> Result<HttpStreamingWav<S>, NewHttpWavError> {
+    let reader = StreamDownload::from_stream(
+        HttpStream::new(client, url).await?,
         MemoryStorageProvider,
         stream_download::Settings::default(),
     )
@@ -28,6 +33,9 @@ pub async fn new_http_wav<S: WavSample>(url: Url) -> Result<HttpStreamingWav<S>,
 pub enum NewHttpWavError {
     #[error("Error decoding the streamed wav file.")]
     WavDecodingError(#[from] StreamingWavError),
+
+    #[error("Error in making the request/getting a response.")]
+    DownloadError(#[from] HttpStreamError<Client>),
 
     #[error("Error initializing the stream.")]
     StreamInitializationError(#[from] StreamInitializationError<HttpStream<Client>>),
